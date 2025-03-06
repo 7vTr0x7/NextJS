d5f287d087427def94c42c063a5c919db8f7bb2f
7vtr0x7 <7vtrox7@gmail.com>
2025-03-07 02:56:54 +0530

    routes and navigation

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,5 +1,4 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,8 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
