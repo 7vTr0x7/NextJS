382e712f7065813f8eab22c299bc53da74fb1d70
7vtr0x7 <7vtrox7@gmail.com>
2025-03-08 16:17:15 +0530

    custom layout

---
+++ frontend/app/services/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ServicesLayout = ({ children }) => {
+  return (
+    <div>
+      <h2>layout</h2>
+      {children}
+    </div>
+  );
+};
+
+export default ServicesLayout;
